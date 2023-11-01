6561a801acaf065c7afd0beb1ef3057d081e847d
macosecheverria <marcosdarkecheverria12@gmail.com>
2023-11-01 13:43:38 -0300

    Subiendo cambios

--- POO/src/Automovil.java
+++ POO/src/Automovil.java
@@ -1,28 +1,96 @@
 public class Automovil {
-    String fabricante;
-    String modelo;
-    String color;
-    double cilindrada;
+    private String fabricante;
+    private String modelo;
+    private String color;
+    private double cilindrada;
+    private int capacidadTanque;
 
-    public String verDetalle(){
+    public Automovil() {
+    }
+
+    public Automovil(String fabricante, String modelo, String color) {
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.color = color;
+    }
+
+    public Automovil(String fabricante, String modelo, String color, double cilindrada) {
+        this(fabricante, modelo, color);
+        this.cilindrada = cilindrada;
+    }
+
+    public Automovil(String fabricante, String modelo, String color, double cilindrada, int capacidadTanque) {
+        this(fabricante, modelo, color, cilindrada);
+        this.capacidadTanque = capacidadTanque;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(double cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public int getCapacidadTanque() {
+        return capacidadTanque;
+    }
+
+    public void setCapacidadTanque(int capacidadTanque) {
+        this.capacidadTanque = capacidadTanque;
+    }
+
+    public String acelerar(int rpm) {
+        return "El auto" + fabricante + "esta acelerando a " + rpm;
+    }
+
+    public String frenar() {
+        return fabricante + " " + modelo + " frenando";
+    }
+
+    public String acelerarFrenar(int rpm) {
+        String acelerar = this.acelerar(2000);
+        String frenar = this.frenar();
+        return acelerar + "\n" + frenar;
+    }
+
+    public float calcularConsumo(int km, float porcentajeGas) {
+        return km / (capacidadTanque * porcentajeGas);
+    }
+
+    public float calcularConsumo(int km, int porcentajeGas) {
+        return km / (capacidadTanque * (porcentajeGas / 100f));
+    }
+
+    public String verDetalle() {
         return "auto.fabricante = " + this.fabricante +
                 "auto.modelo = " + this.modelo +
                 "auto.color = " + this.color +
                 "auto.cilindrada = " + this.cilindrada;
-    }
-
-    public String acelerar(int rpm){
-        return "El auto" + fabricante + "esta acelerando a " + rpm;
-    }
-
-    public String frenar(){
-        return fabricante + " " + modelo + " frenando";
-    }
-
-    public String acelerarFrenar(int rpm){
-        String acelerar = this.acelerar(2000);
-        String frenar = this.frenar();
-        return acelerar + "\n" + frenar;
     }
 
 }
--- POO/src/EjemploAutomovil.java
+++ POO/src/EjemploAutomovil.java
@@ -1,21 +1,50 @@
 public class EjemploAutomovil {
     public static void main(String[] args) {
-        Automovil subaru = new Automovil();
-        subaru.fabricante = "Subaru";
-        subaru.modelo = "Impreza";
-        subaru.color = "Azul";
-        subaru.cilindrada = 2.0;
+        Automovil subaru = new Automovil("Subaru", "Impreza", "azul", 2.0, 40);
+        System.out.println(subaru.getFabricante());
+        System.out.println(subaru.getModelo());
+        System.out.println(subaru.getColor());
+        System.out.println(subaru.getCilindrada());
+        System.out.println(subaru.getCapacidadTanque());
         System.out.println(subaru.acelerar(70));
         System.out.println(subaru.verDetalle());
         System.out.println(subaru.acelerarFrenar(3000));
 
         Automovil mazda = new Automovil();
-        mazda.fabricante = "Mazda";
-        mazda.modelo = "Rx-7";
-        mazda.color = "Rojo";
-        mazda.cilindrada = 3.0;
+        mazda.setFabricante("Mazda");
+        mazda.setModelo("Rx7");
+        mazda.setColor("Rojo");
+        mazda.setCilindrada(4.0);
+        mazda.setCapacidadTanque(70);
+        System.out.println(subaru.getFabricante());
+        System.out.println(subaru.getModelo());
+        System.out.println(subaru.getColor());
+        System.out.println(subaru.getCilindrada());
+        System.out.println(subaru.getCapacidadTanque());
         System.out.println(subaru.acelerar(299));
         System.out.println(mazda.verDetalle());
         System.out.println(mazda.acelerarFrenar(5000));
+        System.out.println("Kilometros por litro " + subaru.calcularConsumo(300, 0.75f));
+        System.out.println("Kilometros por litro " + subaru.calcularConsumo(300, 60));
+
+        Automovil nissan = new Automovil("Nissan", "Skyline Gtr R34", "Azul/Blanco", 3.5, 40);
+        System.out.println(nissan.getFabricante());
+        System.out.println(nissan.getModelo());
+        System.out.println(nissan.getColor());
+        System.out.println(nissan.getCilindrada());
+        System.out.println(nissan.getCapacidadTanque());
+
+
+        Automovil toyota = new Automovil("Toyota", "Supra", "Gris");
+        toyota.setCilindrada(5.0);
+        toyota.setCapacidadTanque(56);
+
+        System.out.println(toyota.getFabricante());
+        System.out.println(toyota.getModelo());
+        System.out.println(toyota.getColor());
+        System.out.println(toyota.getCilindrada());
+        System.out.println(toyota.getCapacidadTanque());
+
+
     }
 }
